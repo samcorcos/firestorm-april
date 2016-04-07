a75e62736007000b70fa9ce3a3c5a5aa4a09c474
Sam Corcos <sam@corcos.io>
2016-04-07 10:11:23 -0400

    relay

--- frontend/app/components/Home/index.js
+++ frontend/app/components/Home/index.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import Relay from 'react-relay'
 
-const Home = () => {
-  return <div> Home </div>
+class Home extends React.Component {
+  render() {
+    return (
+      <div> content </div>
+    )
+  }
+}
+
+Home = Relay.createContainer(Home, {
+  fragments: {
+    store: () => Relay.QL`
+      fragment on HackerNewsAPI {
+        item(id: 8863) {
+          title,
+          score,
+          url,
+          by {
+            id
+          }
+        }
+      }
+    `
+  }
+})
+
+class HackerNewsRoute extends Relay.Route {
+  static routeName = 'HackerNewsRoute'
+  static queries = {
+    store: ((Component) => {
+      return Relay.QL`
+      query root {
+        hn { ${Component.getFragment('store')} },
+      }
+    `}),
+  }
 }
 
 export default Home
